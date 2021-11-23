0439e0264d141053922dd12864cdf7ce56c28d82
MichaelDelcambre <delcambrem@gmail.com>
2021-11-23 17:56:17 -0600

    commit tree dea09fc2345a0d879fe892d505a3ea4822b2da1c

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+
+
+var customerName = 'bob';
+
+function upperCaseCustomerName () {
+   customerName = customerName.toUpperCase();
+} 
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+const leastFavoriteCustomer = 'eleni';
+
+function changeLeastFavoriteCustomer() {
+   leastFavoriteCustomer = 'someone';
+} 
